31f05ec9450fcc90ffcfeb9857e0f35cd620a130
Evan Miller <evandelaymiller12@gmail.com>
2019-12-17 17:26:03 -0600

    Style Changes

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 window.onload = function() {
   this.console.log("Site load successful");
- this.watchUser();
+  this.watchUser();
   this.newSearch();
-
-
 };
 //store
 const key = "mmJ36bPSxG67C3rNpvsYeWcT1TlA5wdf";
@@ -12,87 +10,79 @@ let row = 15;
 let currentParams = null;
 let currentPage = 1;
 let currentSearch = {
-    userYear : null,
-    userMake : null,
-    userModel : null,
-    userLoc : location,
-    dealerType: null,
-}
+  userYear: null,
+  userMake: null,
+  userModel: null,
+  userLoc: location,
+  dealerType: null
+};
 let userPrice = null;
 let userMiles = null;
 let userType = null;
-let userBody = null
+let userBody = null;
 let userTrims = null;
 let distance = 200;
 
 function watchUser() {
-  $('#fsbo , #dealer').on('click', function(){
+  $("#fsbo , #dealer").on("click", function() {
     currentSearch.dealerType = $(this).val();
-    console.log(currentSearch.dealerType)
-    $('.dealerTypeContainer').hide();
-    $('.searchInput').show();
-  })
+    $(".dealerTypeContainer").hide();
+    $(".searchInput").show();
+  });
 
   $(".mainSearch").on("submit", function(e) {
     e.preventDefault();
     currentPage = 1;
     start = 0;
     row = 15;
-    currentSearch.userYear = $('#year');
-    currentSearch.userMake = $('#make') ;
-    currentSearch.userModel = $('#model');
-    currentSearch.userLoc = $('#city');
-   
+    currentSearch.userYear = $("#year");
+    currentSearch.userMake = $("#make");
+    currentSearch.userModel = $("#model");
+    currentSearch.userLoc = $("#city");
 
-
-    getCordinates($('#city'));
+    getCordinates($("#city"));
     updateSearchBar();
     filterToggle();
   });
 }
 
-function setParams(cordinates) { 
-    const params = {
-        year: "&year=" + currentSearch.userYear.val(),
-        make: "&make=" + currentSearch.userMake.val(),
-        model: "&model=" + currentSearch.userModel.val(),
-        distance: currentSearch.dealerType == 2 ? "&radius=" + 200 : '',
-        loc: cordinates,
-      };
-      currentParams = params;
+function setParams(cordinates) {
+  const params = {
+    year: "&year=" + currentSearch.userYear.val(),
+    make: "&make=" + currentSearch.userMake.val(),
+    model: "&model=" + currentSearch.userModel.val(),
+    distance: currentSearch.dealerType == 2 ? "&radius=" + 200 : "",
+    loc: cordinates
+  };
+  currentParams = params;
   watchFilters(cordinates);
   getResults(params);
 }
 
-
 function getResultsRetry(params, n) {
-
-    const url = setURL(params);
+  const url = setURL(params);
   fetch(url)
     .then(Response => {
       if (Response.ok) {
         return Response.json();
       }
-    
+
       throw new Error(Response.statusText);
     })
     .then(ResponseJson => {
-      console.log(ResponseJson);
       displayResults(ResponseJson);
     })
     .catch(err => {
-        console.log(err)
+      console.log(err);
       if (n == 1) throw error;
-          $('.results').empty();
-          $('.results').append(`<h1>Server Error: Trying again</h1>`);
-          $('#js-error').text('Server Error: Please try your search again in a few seconds');
-          return getResultsRetry(params, n - 1);
-     
-     
-
+      $(".results").empty();
+      $(".results").append(`<h1>Server Error: Trying again</h1>`);
+      $("#js-error").text(
+        "Server Error: Please try your search again in a few seconds"
+      );
+      return getResultsRetry(params, n - 1);
     });
 }
-
 
 function getResults(params, n) {
   const url = setURL(params);
@@ -101,27 +91,29 @@ function getResults(params, n) {
       if (Response.ok) {
         return Response.json();
       }
-    
+
       throw new Error(Response.statusText);
     })
     .then(ResponseJson => {
-      console.log(ResponseJson);
       displayResults(ResponseJson);
-      $('.resultsPage').css('display', 'flex');
-      $('.mainPage').hide();
+      $(".resultsPage").css("display", "flex");
+      $(".mainPage").hide();
     })
     .catch(err => {
-        console.log(err)
-      $('.results').empty();
-      $('#js-error').text('Server Error: Please try your search again in a few seconds');
-
+      console.log(err);
+      $(".results").empty();
+      $("#js-error").text(
+        "Server Error: Please try your search again in a few seconds"
+      );
     });
 }
 
 function setURL(params) {
-  console.log(params);
   let apiSearch =
-    "https://marketcheck-prod.apigee.net/v1/search" + getDealerType() + "api_key=" + key;
+    "https://marketcheck-prod.apigee.net/v1/search" +
+    getDealerType() +
+    "api_key=" +
+    key;
   let rows = `&start=${start}&rows=${row}`;
   let sort = `&sort_order=asc`;
   let url = userOptions() + rows + sort;
@@ -134,25 +126,23 @@ function setURL(params) {
     return apiSearch;
   }
 
-  console.log(url);
   return url;
 }
 
 function getDealerType() {
-  console.log('Dealertype is' + currentSearch.dealerType)
-  if (currentSearch.dealerType == 1 ){
-    return '/fsbo?'; 
+  if (currentSearch.dealerType == 1) {
+    return "/fsbo?";
   } else {
-    return '?';
+    return "?";
   }
-  
-} 
+}
 function displayResults(ResponseJson) {
   $(".results").empty();
-  console.log(ResponseJson.listings.length);
-$('#range_disp').text(distance); 
+  $("#range_disp").text(distance);
   $(".numberResults").html(
-    `Search Results: <mark class="number">${ResponseJson.num_found < 15 ? '': start+15 + ' of '}(${ResponseJson.num_found})</mark>`
+    `Search Results: <mark class="number">${
+      ResponseJson.num_found < 15 ? "" : start + 15 + " of "
+    }(${ResponseJson.num_found})</mark>`
   );
   for (let i = 0; i < ResponseJson.listings.length; i++) {
     let j = ResponseJson.listings[i];
@@ -172,7 +162,6 @@ $('#range_disp').text(distance);
       dealerWeb: j.vdp_url
     };
 
-  //  $('.filtersArea , .displayArea, .filterToggle').toggleClass('on');
     $(".results").append(`<li class="resultFrame">
         <section class="pictureContainer">
                <img class="image" src="${DATA.imgsrc}" />
@@ -210,16 +199,16 @@ $('#range_disp').text(distance);
 </section>
        </li>`);
   }
-  if (ResponseJson.num_found > 15){
-  $(".results").append(
-    `<li class='pageLi'><section class="pageSection">
+  if (ResponseJson.num_found > 15) {
+    $(".results").append(
+      `<li class='pageLi'><section class="pageSection">
     <div class="button-container"> 
     <button class="pagePrev">Previous Page</button>
     <label for='current-page' class='pageNumber'>Page: ${currentPage}</label>
    <button class="pageNext">Next Page</button>
 </div>
 </section></li>`
-  );
+    );
   }
   setTrims(ResponseJson);
   watchPages();
@@ -243,7 +232,10 @@ function watchFilters(cordinates) {
       loc: cordinates,
       trim: getTrims(),
       bodyTypes: getBodyTypes(),
-      distance: currentSearch.dealerType == 2 ? "&radius=" + $("#range_disp").val() : '',
+      distance:
+        currentSearch.dealerType == 2
+          ? "&radius=" + $("#range_disp").val()
+          : "",
       priceRange: getPrice(),
       milesRange: getMiles(),
       condition: "&car_type=" + getType()
@@ -251,7 +243,6 @@ function watchFilters(cordinates) {
 
     currentParams = newParams;
     distance = $("#range_disp").val();
-    console.log(newParams);
     getResultsRetry(newParams);
     updateSearchBar();
     $(".filtersArea, .displayArea, .filterToggle").toggleClass("on");
@@ -259,10 +250,9 @@ function watchFilters(cordinates) {
 
   function getTrims() {
     let trim = "&trim=";
-    let select = '';
-    //(!check if option checked)
+    let select = "";
     $('input[name="trimoption"]:checked').each(function() {
-      select += ' '+ this.value + ",";
+      select += " " + this.value + ",";
       trim += this.value + ",";
     });
 
@@ -273,15 +263,13 @@ function watchFilters(cordinates) {
 
   function getBodyTypes() {
     let type = "&body_type=";
-    let type1 = 'body type: '
-    //(!check if option checked)
+    let type1 = "body type: ";
     $('input[name="bodytype"]:checked').each(function() {
-        type1 += this.value + ", ";
+      type1 += this.value + ", ";
       type += this.value + ",";
     });
     userBody = type1.substring(0, type1.length - 2);
     let fullstring = type.substring(0, type.length - 1);
-    console.log('this is fullstring' + fullstring)
     return fullstring;
   }
 
@@ -292,7 +280,6 @@ function watchFilters(cordinates) {
       return "used";
     }
 
-    console.log(selected);
     userType = selected;
     return selected;
   }
@@ -311,7 +298,7 @@ function watchFilters(cordinates) {
     } else {
       max = $(".maxMiles").val();
     }
-    userMiles = `${min}-${max}`
+    userMiles = `${min}-${max}`;
     return `&miles_range=${min}-${max}`;
   }
 
@@ -319,7 +306,6 @@ function watchFilters(cordinates) {
     let min = null;
     let max = null;
 
-    console.log("this is min" + $(".minPrice").val());
     if ($(".minPrice").val() == "" || null || undefined) {
       min = 0;
     } else {
@@ -331,15 +317,14 @@ function watchFilters(cordinates) {
     } else {
       max = $(".maxPrice").val();
     }
-    userPrice = `${min}-${max}`
+    userPrice = `${min}-${max}`;
     return `&price_range=${min}-${max}`;
   }
 }
 
 function setTrims(ResponseJson) {
-    $('.trims').empty();
+  $(".trims").empty();
   let trims = [];
-  console.log(" trims ran");
   for (let i = 0; i < ResponseJson.listings.length; i++) {
     let t = ResponseJson.listings[i].build.trim;
     if (t !== undefined || " ") trims.push(t);
@@ -354,7 +339,6 @@ function setTrims(ResponseJson) {
     );
   });
 }
-
 
 /* Google maps API */
 function initialize() {
@@ -373,36 +357,33 @@ function getCordinates(loc) {
   let url =
     "https://maps.googleapis.com/maps/api/geocode/json?address=" + params + key;
 
-    //fetch location with Google places API
+  //fetch location with Google places API
   fetch(url)
     .then(Response => {
       if (Response.ok) {
         return Response.json();
       }
       throw new Error(Response.statusText);
-
     })
     .then(ResponseJson => {
-      console.log(ResponseJson);
       let lat = ResponseJson.results[0].geometry.location.lat;
       let lng = ResponseJson.results[0].geometry.location.lng;
       cordinates = `&latitude=${lat}&longitude=${lng}`;
       setParams(cordinates);
     })
     .catch(function(err) {
-        console.log(err)
-      $('.results').empty();
-      $('#js-error').text('Server Error: Please try your search again in a few seconds');
+      $(".results").empty();
+      $("#js-error").text(
+        "Server Error: Please try your search again in a few seconds"
+      );
     });
 }
 
 function watchPages() {
   $(".pageNext").on("click", function(e) {
-    console.log("nextPage");
     updateResultsNext();
   });
   $(".pagePrev").on("click", function(e) {
-    console.log("prevPage");
     updateResultsPrev();
   });
 }
@@ -410,7 +391,6 @@ function watchPages() {
 function updateResultsNext() {
   start += 15;
   currentPage++;
-  console.log("new start =" + start);
   getResultsRetry(currentParams);
   $(".displayArea").scrollTo(".resultsLegend");
 }
@@ -419,7 +399,6 @@ function updateResultsPrev() {
   if (start > 0) {
     start -= 15;
     currentPage--;
-    console.log("prev start =" + start);
     getResultsRetry(currentParams);
     $(".displayArea").scrollTo(".resultsLegend");
   }
@@ -447,107 +426,105 @@ jQuery.fn.scrollTo = function(elem, speed) {
   return this;
 };
 
-
 //updating search bar
 function updateSearchBar() {
-        let userSearch = {
-          dealer: currentSearch.dealerType == 1 ? 'Private Party' : 'Dealership',
-      userBuild : currentSearch.userYear.val() + ' ' + currentSearch.userMake.val() + ' ' + currentSearch.userModel.val(),
-      location : currentSearch.userLoc.val(),
-        trim : userTrims == null ? '' : userTrims.substring(0, userTrims.length - 1),
-      distance1 :'Radius: ' +distance,
-      priceRange :'Price: ' +userPrice,
-      milesRange:'Miles: ' +userMiles,
-      body_type:userBody,
-      condition:userType
-        }
+  let userSearch = {
+    dealer: currentSearch.dealerType == 1 ? "Private Party" : "Dealership",
+    userBuild:
+      currentSearch.userYear.val() +
+      " " +
+      currentSearch.userMake.val() +
+      " " +
+      currentSearch.userModel.val(),
+    location: currentSearch.userLoc.val(),
+    trim: userTrims == null ? "" : userTrims.substring(0, userTrims.length - 1),
+    distance1: "Radius: " + distance,
+    priceRange: "Price: " + userPrice,
+    milesRange: "Miles: " + userMiles,
+    body_type: userBody,
+    condition: userType
+  };
 
-        cleanSearch();
+  cleanSearch();
   function cleanSearch() {
-     for (let i in userSearch) {
-         switch (userSearch[i]){
-            case null:
-                delete userSearch[i];
-            case '':
-                delete userSearch[i];
+    for (let i in userSearch) {
+      switch (userSearch[i]) {
+        case null:
+          delete userSearch[i];
+        case "":
+          delete userSearch[i];
 
-            case 'Price: 0-10000000': 
-            delete userSearch[i];
+        case "Price: 0-10000000":
+          delete userSearch[i];
 
-            case 'Miles: 0-10000000': 
-            delete userSearch[i];
-            
-            case 'body type':
-            delete userSearch[i];
+        case "Miles: 0-10000000":
+          delete userSearch[i];
 
-            case 'Price: null' :
-            delete userSearch[i];
+        case "body type":
+          delete userSearch[i];
 
-            case 'Miles: null' :
-            delete userSearch[i];
-         }
-        }
+        case "Price: null":
+          delete userSearch[i];
+
+        case "Miles: null":
+          delete userSearch[i];
       }
-        //set current params
-             
-console.log(userSearch)
-
-        $(".labels").empty();
-    let values = Object.values(userSearch);
-    console.log(values)
-    for (let i = 0; i < values.length; i++){
-            $(".labels")
-              .append(`<li class="filterFrame"><span class="filterLabel">${values[i]}</span>
-              </li>`);
-          
     }
-    clearValues();
+  }
+
+  $(".labels").empty();
+  let values = Object.values(userSearch);
+  for (let i = 0; i < values.length; i++) {
+    $(".labels")
+      .append(`<li class="filterFrame"><span class="filterLabel">${values[i]}</span>
+              </li>`);
+  }
+  clearValues();
 }
-function clearValues(){
-    userPrice = null;
-userMiles = null;
-userType = null;
-userBody = null;
-userTrims = null;
+function clearValues() {
+  userPrice = null;
+  userMiles = null;
+  userType = null;
+  userBody = null;
+  userTrims = null;
 }
 
 function filterToggle() {
   $(".filterToggle").on("click", function() {
-    $('.tutorial').hide();
+    $(".tutorial").hide();
     $(".filtersArea, .displayArea, .filterToggle").toggleClass("on");
   });
 }
 
-function newSearch(){
-    $('.newSearch, .labels, .searchIconContainer').on('click', function(){
-        $('.newSearchPage').toggleClass('on');
-        
-    })
+function newSearch() {
+  $(".newSearch, .labels, .searchIconContainer").on("click", function() {
+    $(".newSearchDealerTypeContainer").show();
+    $(".newSearchContainer").hide();
+    $(".newSearchPage").toggleClass("on");
+  });
 
-    $('#newSearchFsbo , #newSearchDealer').on('click', function(){
-      currentSearch.dealerType = $(this).val();
-      console.log(currentSearch.dealerType)
-      $('.newSearchDealerTypeContainer').hide();
-      $('.newSearchContainer').show();
-    })
+  $("#newSearchFsbo , #newSearchDealer").on("click", function() {
+    currentSearch.dealerType = $(this).val();
+    $(".newSearchDealerTypeContainer").hide();
+    $(".newSearchContainer").show();
+  });
 
-    $(".newSearchForm").on("submit", function(e) {
-        e.preventDefault();
-        currentPage = 1;
-        start = 0;
-        row = 15;
-        currentSearch.userYear = $('#yearNewSearch');
-        currentSearch.userMake = $('#makeNewSearch') ;
-        currentSearch.userModel = $('#modelNewSearch');
-        currentSearch.userLoc = $('#cityNewSearch');
+  $(".newSearchForm").on("submit", function(e) {
+    e.preventDefault();
+    currentPage = 1;
+    start = 0;
+    row = 15;
+    currentSearch.userYear = $("#yearNewSearch");
+    currentSearch.userMake = $("#makeNewSearch");
+    currentSearch.userModel = $("#modelNewSearch");
+    currentSearch.userLoc = $("#cityNewSearch");
 
-        if ($('#cityNewSearch').val() !== ''|| undefined || null){
-          getCordinates($('#cityNewSearch'));
-          updateSearchBar();
-          $('.newSearchPage').toggleClass('on');
-        }  else {
-          $('#js-errorNewSearch').text('Please enter a valid location');
-   
-        }
-    })
+    if ($("#cityNewSearch").val() !== "" || undefined || null) {
+      getCordinates($("#cityNewSearch"));
+      updateSearchBar();
+      $(".newSearchPage").toggleClass("on");
+    } else {
+      $("#js-errorNewSearch").text("Please enter a valid location");
+    }
+  });
 }
